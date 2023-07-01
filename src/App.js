32444cc415a4fbaf03ff129ba4dfdf78d808a48f
Francis Echesi <echesisuccess01@gmail.com>
2023-07-01 13:41:08 +0100

    Submitted work

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ import facebook from "./images/facebook.svg";
 import youtube from "./images/youtube.svg";
 import "./index.css";
 import FooterLink from "./components/FooterLink";
+import DonateButton from "./components/DonateButton";
 
 function App() {
   return (
@@ -28,44 +29,55 @@ function App() {
       <div
         id="header"
         className="bg-[#061237]"
-        style={{
-          backgroundImage: `linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.3)),
-            url(${hero})`,
-          backgroundSize: "cover",
-          backgroundRepeat: "no-repeat",
-          backgroundPosition: "center",
-        }}
+
+        //code for the header image (hero image). To be reintroduced in v2
+        // style={{
+        //   backgroundImage: `linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.3)),
+        //     url(${hero})`,
+        //   backgroundSize: "cover",
+        //   backgroundRepeat: "no-repeat",
+        //   backgroundPosition: "center",
+        // }}
       >
         <div id="nav" className="pt-8 mx-8 flex justify-between">
-          <div className="flex items-center gap-2">
-            <img src={logo} width={20} alt="logo" />
-            <span className="text-3xl font-semibold text-[#ffffff]">
-              Steluh
-            </span>
-          </div>
+          <a href="/">
+            <div className="flex items-center gap-2">
+              <img src={logo} width={20} alt="logo" />
+              <span
+                className="text-3xl font-semibold text-[#ffffff]"
+                style={{ fontFamily: "Arial" }}
+              >
+                steluh
+              </span>
+            </div>
+          </a>
           <div className="flex gap-10 items-center">
+            <DonateButton />
+            <a href="/about-us">
+              <NavLink>About Us</NavLink>
+            </a>
             <NavLink>Mentor</NavLink>
             <NavLink>Advisor</NavLink>
             <NavLink>FAQs</NavLink>
-            <NavLink>Contact Us</NavLink>
-            <Button>Sign In</Button>
           </div>
         </div>
 
         <div id="hero" className="mx-8 flex justify-center">
-          <div className="flex flex-col gap-10 items-center w-[80%] my-[100px]">
-            <div className="text-[50px] text-[#ffffff] text-center font-bold font-mono">
-              Enroll in any university, college, or professional program in
-              Africa or its diaspora.
+          <div className="flex flex-col gap-5 items-center w-[95%] my-[20px]">
+            <div
+              className="text-[30px] text-[#ffffff] text-center font-bold font-mono"
+              style={{ fontFamily: "Times New Roman" }}
+            >
+              Enroll in the university, college, or professional program in
+              Africa or its diaspora?
             </div>
-            <button className="w-fit py-4 px-16 bg-[#0cd2dc] hover:bg-[#08838a] text-white text-xl font-semibold border-none rounded-lg cursor-pointer transition-all ease-out duration-300">
-              Apply Here
-            </button>
+            <Button>Apply Here</Button>
           </div>
         </div>
       </div>
 
-      <div id="sponsors" className="flex justify-center">
+      {/*Code for the sponsors. Base code for the past clients in about page */}
+      {/* <div id="sponsors" className="flex justify-center">
         <div className="pt-8 mx-8 bg-[#fffffe] flex flex-col w-[80%]">
           <div className="text-center text-2xl font-semibold text-[#aaaaaa]">
             Sponsors
@@ -79,12 +91,17 @@ function App() {
             <img width={140} src={intel} alt="intel" />
           </div>
         </div>
-      </div>
+      </div> */}
 
-      <div id="competency-frameworks" className="flex justify-center my-10">
-        <div className="pt-8 mx-8 flex flex-row w-[80%] justify-evenly gap-16">
-          <div className="text-6xl font-bold">
+      <div id="competency-frameworks" className="flex justify-center my-6">
+        <div className="mx-8 flex flex-row w-[80%] justify-evenly gap-16">
+          <div className="text-6xl font-bold flex flex-col justify-between">
             Competency <br /> Frameworks
+            <div className="text-xl">
+              <Button className="flex flex-row items-center">
+                Discover More <span className="text-2xl">&#8250;&#8250;</span>
+              </Button>
+            </div>
           </div>
           <div className="grid grid-cols-2 gap-8">
             <div className="flex flex-col gap-3">
@@ -95,12 +112,12 @@ function App() {
                 Development
               </div>
               <div className="text-md text-[#aaaaaa]">
-                We believe in nurturing and fostering students to explore their
-                potential, acquire new skills, and enhance their personal and
-                professional capabilities. Through our comprehensive guidance
-                and support, we aim to help students develop a strong foundation
-                that will serve them well in their academic journey and future
-                career endeavors.
+                By supporting and encouraging students to discover their
+                potential, learn new skills, and improve their talents on a
+                personal and professional level. We seek to assist students in
+                building a solid foundation that will serve them well in their
+                academic path and future career ambitions through our thorough
+                supervision and assistance.
               </div>
             </div>
 
@@ -112,11 +129,11 @@ function App() {
                 Scholarship
               </div>
               <div className="text-md text-[#aaaaaa]">
-                We understand the importance of financial assistance in pursuing
-                higher education. We provide valuable resources and guidance to
-                help students navigate through the scholarship application
-                process, ensuring they have access to the financial support they
-                need to make their educational dreams a reality.
+                We are aware of how crucial financial aid is for higher
+                education. We offer helpful information and direction to assist
+                students in completing the scholarship application process,
+                ensuring they have access to the financial aid they require to
+                realize their aspirations of attending tertiary.
               </div>
             </div>
             <div className="flex flex-col gap-3">
@@ -125,14 +142,14 @@ function App() {
                   <img width={30} src={education} alt="education" />
                 </span>
                 Education
-              </div>{" "}
+              </div>
               <div className="text-md text-[#aaaaaa]">
-                Education is the cornerstone of personal and societal growth. We
-                offer comprehensive guidance on choosing the right colleges or
-                universities, selecting suitable programs, and understanding the
-                admission requirements. Our aim is to provide students with the
-                knowledge and tools they need to make the most of their
-                educational opportunities.
+                Education is the foundation for both individual and society
+                development. We provide in-depth advice on picking the best
+                schools or universities, appropriate programs, and entrance
+                criteria. Our goal is to arm students with the information and
+                resources they require to fully benefit from their educational
+                prospects.
               </div>
             </div>
             <div className="flex flex-col gap-3">
@@ -143,84 +160,34 @@ function App() {
                 Empowerment
               </div>
               <div className="text-md text-[#aaaaaa]">
-                We strongly believe in empowering students to become
-                independent, confident individuals who can navigate the
-                complexities of the college and university application process.
-                We equips students with essential life skills, such as effective
-                communication, critical thinking, and self-advocacy.
+                As steadfast advocates of giving students the tools they need to
+                develop into self-reliant, self-assured adults who can
+                successfully negotiate the complexity of the college and
+                university admission process. We provide you with the tools
+                needed to succeed in life, including effective communication,
+                critical thinking, and self-advocacy.
               </div>
             </div>
           </div>
         </div>
       </div>
 
-      <div
-        id="about-us"
-        className="flex justify-center my-10 py-28 bg-[#fafafa]"
-      >
-        <div className="mx-8 flex flex-row w-[80%] justify-evenly gap-16">
-          <div className="flex items-center rounded-lg w-[2500px] overflow-hidden">
-            <iframe
-              width="600"
-              height="400"
-              src="https://www.youtube.com/embed/BIpUzT4ozXs?list=TLGGwW_2bTbJH4UzMDA2MjAyMw"
-              title="AUCAS.org"
-              frameborder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              allowfullscreen
-            ></iframe>
-          </div>
-
-          <div className="text-md text-[#aaaaaa] flex flex-col gap-10">
-            <div className="text-6xl font-bold text-[#000000]">About Us</div>
-            Operating for more than four generations, having been there for a
-            while, and having a rich legacy, we are known across the world for
-            affecting transformation and have a long history of empowering
-            people. When we became official in June 2017, we underwent a key
-            turning point or reorganization to broaden or refocus our purpose
-            and objectives.
-            <br />
-            <br />
-            We have a physical presence in Accra, Ghana, and a connection to the
-            neighborhood. We have actively participated in the region's growth
-            and empowerment by supporting its development and progress. We are
-            known all over the world for our knowledge and influence as "a
-            world-leading entity dedicated to empowering individuals and
-            communities."
-          </div>
-        </div>
-      </div>
-
-      <div id="banner" className="flex justify-center py-16">
-        <div
-          className="mx-8 w-[80%] flex flex-col items-center justify-center gap-16 bg-[#1D5BFE] py-16 rounded-lg"
-          style={{
-            backgroundImage: `linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.3)),
-            url(${banner})`,
-          }}
-        >
-          <div className="text-4xl text-center font-bold text-[#ffffff] w-[80%]">
-            Embark on a Journey of Knowledge: <br />
-            Enroll in African Institutions and Beyond
-          </div>
-          <button className="w-fit py-4 px-16 bg-[#0cd2dc] hover:bg-[#08838a] text-white text-xl font-semibold border-none rounded-lg cursor-pointer transition-all ease-out duration-300">
-            Get In Touch
-          </button>
-        </div>
-      </div>
-
-      <div id="footer" className="py-8 bg-[#061237]">
+      {/*Footer code.  */}
+      {/* <div id="footer" className="pb-2 pt-8 bg-[#061237]">
         <div className="flex flex-row mx-40 justify-between">
           <div className="flex flex-col gap-2">
             <div className="flex items-center gap-2">
               <img src={logo} width={20} alt="logo" />
-              <span className="text-3xl font-semibold text-[#ffffff]">
-                Steluh
+              <span
+                className="text-3xl font-semibold text-[#ffffff]"
+                style={{ fontFamily: "Arial" }}
+              >
+                steluh
               </span>
             </div>
             <div className="text-md text-[#aaaaaa] w-[300px]">
-              Enroll in any university, college, or professional program in
-              Africa or its diaspora.
+              Enroll in the university, college, or professional program in
+              Africa or its diaspora?
             </div>
             <div className="flex flex-row justify-between">
               <img
@@ -295,6 +262,17 @@ function App() {
             </div>
           </div>
         </div>
+        <br />
+        <hr style={{ color: "#aaaaaa", height: "0.2px" }} />
+        <div className="text-[#aaaaaa] text-md text-center pt-2">
+          All Rights Reserved <br />
+          steluh &copy; 2017
+        </div>
+      </div> */}
+      <hr style={{ color: "#aaaaaa", height: "0.2px" }} />
+      <div className="text-[#aaaaaa] text-md text-center pt-2">
+        All Rights Reserved <br />
+        steluh &copy; 2017
       </div>
     </div>
   );
---
+++ src/components/DonateButton.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DonateButton = () => {
+  return (
+    <button className="w-fit py-2 px-10 bg-[#ff8000] hover:bg-[#ff8000aa] text-white text-md border-none rounded-lg cursor-pointer transition-all ease-out duration-300">
+      DONATE
+    </button>
+  );
+};
+
+export default DonateButton;
